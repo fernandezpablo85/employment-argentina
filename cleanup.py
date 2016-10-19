bbecdbf33e941190640432ebe5b8dda47144485a
Pablo Fernandez <fernandezpablo85@gmail.com>
2016-10-19 12:27:26 -0400

    generalize to multiple factors

--- cleanup.py
+++ cleanup.py
@@ -1,7 +1,15 @@
-provinces = ['CABA', 'Buenos Aires', 'Catamarca', 'Cordoba', 'Corrientes', 'Chaco', 'Chubut',
-             'Entre Rios', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquen',
-             'Rio Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero',
-             'Tierra del Fuego', 'Tucuman', 'NA']
+factors = [
+    {
+        'name': 'province',
+        'data': ['CABA', 'Buenos Aires', 'Catamarca', 'Cordoba', 'Corrientes', 'Chaco', 'Chubut',
+                 'Entre Rios', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquen',
+                 'Rio Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero',
+                 'Tierra del Fuego', 'Tucuman', 'NA']
+    }, {
+        'name': 'company_size',
+        'data': ['0-10', '11-50', '51-200', '201-1000', '1001+']
+    }
+]
 
 quarters = ['Q1-04', 'Q2-04', 'Q3-04', 'Q4-04', '04', 'Q1-05', 'Q2-05', 'Q3-05', 'Q4-05', '05',
             'Q1-06', 'Q2-06', 'Q3-06', 'Q4-06', '06', 'Q1-07', 'Q2-07', 'Q3-07', 'Q4-07', '07', 'Q1-08',
@@ -9,6 +17,10 @@ quarters = ['Q1-04', 'Q2-04', 'Q3-04', 'Q4-04', '04', 'Q1-05', 'Q2-05', 'Q3-05',
             'Q3-10', 'Q4-10', '10', 'Q1-11', 'Q2-11', 'Q3-11', 'Q4-11', '11', 'Q1-12', 'Q2-12', 'Q3-12',
             'Q4-12', '12', 'Q1-13', 'Q2-13', 'Q3-13', 'Q4-13', '13', 'Q1-14', 'Q2-14', 'Q3-14', 'Q4-14', '14',
             'Q1-15', 'Q2-15', 'Q3-15', 'Q4-15', '15', 'Q1-16', 'Q2-16']
+
+
+def get_factor_by_name(name):
+    return next(f for f in factors if f['name'] == factor)
 
 
 def remove_thousands_separator(lines):
@@ -23,22 +35,23 @@ def is_quarter(string):
     return string.startswith('Q')
 
 
-def format_csv(entry):
+def format_csv(factor_name, entry):
     """Format a data entry as a csv line."""
-    return "%s, %s, %s" % (entry['province'], entry['quarter'], entry['count'])
+    return "%s, %s, %s" % (entry[factor_name], entry['quarter'], entry['count'])
 
 
-def format_json(entry):
+def format_json(factor_name, entry):
     """Format a data entry as a json object."""
-    return '{ "province": "%s", "quarter": "%s", "count": "%s" }' % (
-        entry['province'], entry['quarter'], entry['count'])
+    return '{ "%s": "%s", "quarter": "%s", "count": "%s" }' % (
+        factor_name, entry[factor_name], entry['quarter'], entry['count'])
 
 
-def create_data_structure(lines):
-    """Add province and quarter data to measurements."""
+def create_data_structure(lines, factor_name):
+    """Add chosen factor data to measurements."""
     data = []
     for i, line in enumerate(lines):
-        province = provinces[i]
+        factor_chosen = get_factor_by_name(factor_name)
+        factor = factor_chosen['data'][i]
         measures = line.split()
         for j, m in enumerate(measures):
             quarter = quarters[j]
@@ -46,10 +59,12 @@ def create_data_structure(lines):
             # avoid year means.
             if not is_quarter(quarter):
                 continue
-            data.append({'province': province, 'quarter': quarter, 'count': m})
+            data.append({factor_chosen['name']: factor, 'quarter': quarter, 'count': m})
     return data
 
+
 import sys
+from functools import partial
 
 # check if we have standard input.
 if sys.stdin.isatty():
@@ -58,8 +73,10 @@ if sys.stdin.isatty():
 
 lines = sys.stdin.readlines()
 no_separator = remove_thousands_separator(lines)
-data = create_data_structure(no_separator)
 
-csv = map(format_csv, data)
+factor = 'province'
+data = create_data_structure(no_separator, factor)
+fmt = partial(format_csv, factor)
+csv = map(fmt, data)
 for item in csv:
     print item
